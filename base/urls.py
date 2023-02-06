cfa1412acfbc9282d0afc09ba60f8d213fbf62a7
Solas <justinesolas23@gmail.com>
2023-02-06 09:41:48 +0800

    'updated-design'

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('patient-details/<int:pk>', views.patient_details, name='patient-details'),
     path('patient-form-update/<int:pk>', views.patient_form_update, name='patient-form-update'),
     path('patient-data/<int:pk>', views.patient_data, name='patient-data'),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
